c0af35af9a70380d32370675ae7c013ff975a9ff
Bard <nigel.terpstra@student.nhlstenden.com>
2023-12-12 15:50:53 +0100

    small change to exception_handling, std::exception has not ctor that handles messages.

--- BardCore/include/BardCore/bardcore.h
+++ BardCore/include/BardCore/bardcore.h
@@ -2,16 +2,16 @@
 #define BARDCORE_BARDCORE_H
 
 #if __cplusplus >= 201703L
-    #define CXX17
+#define CXX17
 #endif
 
 #if __cplusplus >= 202002L
-    #define CXX20
+#define CXX20
 #endif
 
 #ifdef CXX17 // C++17 or later
-    #define NODISCARD [[nodiscard]]
-    #define INLINE inline
+#define NODISCARD [[nodiscard]]
+#define INLINE inline
 #else
     #define INLINE 
     #define NODISCARD
@@ -21,17 +21,36 @@
 #include <ostream>
 #include <exception>
 #ifdef CXX17
-    #include <optional>
+#include <optional>
 #endif // CXX17
 
 namespace bardcore
 {
     namespace exceptions
     {
+        /// \brief base class for all exceptions in BardCore
         class bard_exception : public std::exception
         {
+        protected:
+            std::string msg_;
+
         public:
-            using std::exception::exception;
+            explicit bard_exception(const char* msg) : msg_(msg)
+            {
+            }
+
+            explicit bard_exception(const std::string& msg): msg_(msg)
+            {
+            }
+
+            ~bard_exception() noexcept override = default;
+
+            /// \brief returns the message of the exception
+            /// \return the message of the exception
+            NODISCARD const char* what() const noexcept override
+            {
+                return msg_.c_str();
+            }
         };
     } // namespace bardcore::exceptions
 } // namespace bardcore
